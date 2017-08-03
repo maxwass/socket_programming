ee8b49dc838eb0222c8bd003d2140b142e135352
Max <maxwass@seas.upenn.edu>
2017-08-03 13:59:21 -0400

    started server side. Can succesfully accept client connection requests

--- server.c
+++ server.c
@@ -11,38 +11,30 @@
 #include <arpa/inet.h> //convert ip address to human readable (and back)
 #include <netdb.h>
 
-//Helpful resource: http://beej.us/guide/bgnet/output/print/bgnet_USLetter.pdf
-
 int main ( int argc, char *argv[] ) {
  
     //parse CLI
-    assert(argc == 4 && "Incorrect number of Arguments to client: server ip, port #, string message");
-    char *server_ip_cli = argv[1]; //server ip address ?what would happen if char a = *arg...
-    char *port      = argv[2]; //port number
-    char *msg       = argv[3]; //message to be sent
+    assert(argc == 2 && "Incorrect number of Arguments to client: server ip, port #, string message");
+    char *port      = argv[1]; //port number
     
     //CLI processing
     struct sockaddr_in server_addr;
     char str[INET_ADDRSTRLEN];
 
-    // store this IP address in sockadd structure:
-    inet_pton(AF_INET, server_ip_cli, &(server_addr.sin_addr));
-    // now get it back and put into str
-    inet_ntop(AF_INET, &(server_addr.sin_addr), str, INET_ADDRSTRLEN); //STILL SOME PROBLEMS WITH STR
-    //assert(!strcmp(server_ip_cli, str) && "ip addr string-> int error");
-    printf("CLI Inputs: \n   server_ip: %s, port: %s, msg: %s \n\n", server_ip_cli, port, msg);
+    printf("CLI Input: \n  port: %s\n\n", port);
 
-    
-    //get server address info with getaddrinfo()
+   
+    //get local address info with getaddrinfo()
     int status;
-    struct addrinfo hints, *rp, *res;
-    
+    struct addrinfo hints, *rp;
+    struct addrinfo *servinfo;
+
     memset(&hints, 0, sizeof(hints)); // make sure the struct is empty
     hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
     hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
     hints.ai_flags = AI_PASSIVE;     // fill in my IP for me = HOST MACHINE IP
-    // If you want to bind to a specific local IP address, drop the AI_PASSIVE and put an IP address in for the first argument to getaddrinfo().
-    if ((status = getaddrinfo(server_ip_cli, port, &hints, &res)) != 0) {
+
+    if ((status = getaddrinfo(NULL, port, &hints, &servinfo)) != 0) {
             fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
                 exit(1);
     }
@@ -50,7 +42,7 @@ int main ( int argc, char *argv[] ) {
     /* loop over all returned results and do inverse lookup */
     printf("Printing hostnames returned from getaddrinfo() in linked list: \n\n");
     int error, sockfd;
-    for (rp = res; rp != NULL; rp = rp->ai_next)
+    for (rp = servinfo; rp != NULL; rp = rp->ai_next)
     { 
         //reads addrinfo structures, looks up addr, checks local files for ip/hostname mappings, prints out
         char hbuf[NI_MAXHOST], sbuf[NI_MAXSERV];
@@ -62,16 +54,15 @@ int main ( int argc, char *argv[] ) {
             printf("FAIL HOSTNAME LOOKUP!\n\n\n");
         }
 
-        //trying to connect to socket
+        //trying to create socket
         sockfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
-        int c = connect(sockfd, rp->ai_addr, rp->ai_addrlen);
         
-        if( sockfd !=-1 && c !=-1){
-            printf("Connection success!! sockfd: %i, connect: %i\n", sockfd, c);
+        if( sockfd !=-1 ){
+            printf("Created socket!!, sockfd: %i\n", sockfd);
             break;
         }
         else{
-            printf("Could not Connect.   sockfd: %i, connect: %i\n", sockfd, c);
+            printf("Could not create socket!!, sockfd: %i\n", sockfd);
         }
  
         printf("\n");
@@ -79,20 +70,42 @@ int main ( int argc, char *argv[] ) {
 
 
     if (rp == NULL) {               /* No address succeeded */
-            fprintf(stderr, "Could not connect to any returned hostname\n");
+            fprintf(stderr, "Could not create socket with any returned hostname\n");
             exit(EXIT_FAILURE);
             
     }
 
-    //send data
-    int n = send(sockfd, msg, strlen(msg), 0);
-    printf("\nSend: %i/%i bytes written\n", n ,(int)strlen(msg));
+    //bind socket to local address (port)
+    if(bind(sockfd, rp->ai_addr, rp->ai_addrlen)< 0) {perror("ERROR on binding");}
+
+    listen(sockfd, 10); //listen for up to 10 connnections on this port
+
+    struct sockaddr_storage their_addr;
+    int addr_size;
+    addr_size = sizeof(their_addr);
     
+    int new_sockfd;
+    new_sockfd = accept(sockfd, (struct sockaddr *)&their_addr, (socklen_t *)&addr_size);
+    if(sockfd < 0) perror("Error on accepting connection!");
+    else printf("Successfully Accepted Connection: %i \n", new_sockfd);
+
+
+
+/*
+
+
     char read_buffer[100];
     int r = read(sockfd,read_buffer,100);
     printf("\nRead: %s \n bytes read %i \n",read_buffer, r);
     
+
+    //send data
+    char reply[100];
+    strcpy( strcat("I recieved: ", read_buffer), reply);
+    int n = send(sockfd, reply, strlen(reply), 0);
+    printf("\nSend: %i/%i bytes written\n", n ,(int)strlen(reply));
     
+*/
     //freeaddrinfo(res); freeaddrinfo(&hints); freeaddrinfo(rp);
 }
 
